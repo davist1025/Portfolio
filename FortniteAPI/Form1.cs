debb676757a0c84951c96d933f88286444ae392c
Tyler <davistglenn@gmail.com>
2024-02-25 22:17:22 -0500

    [FortniteAPI] - Started implementation.

--- FortniteAPI/Form1.cs
+++ FortniteAPI/Form1.cs
@@ -2,9 +2,23 @@ namespace FortniteAPI
 {
     public partial class Form1 : Form
     {
+        private FortniteClient _client;
+
         public Form1()
         {
             InitializeComponent();
+
+            _client = new FortniteClient();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await _client.GetPlaylists();
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            await _client.GetShop();
         }
     }
 }
